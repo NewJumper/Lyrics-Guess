4412c536e2adacd4b65b36c218b0ac25f6e42bcd
muhta <muhtasim.nabi@gmail.com>
2023-05-05 21:40:56 -0500

    Initial commit

---
+++ src/main/java/gui/QuitMenu.java
@@ -0,0 +1,37 @@
+package gui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class QuitMenu {
+    public static Stage window;
+
+    public static void display() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(QuitMenu.class.getResource("quit.fxml")));
+        Stage stage = new Stage();
+        window = stage;
+        stage.setTitle("Quit?");
+        stage.getIcons().add(new Image("gui/textures/quit-icon.png"));
+        stage.initModality(Modality.APPLICATION_MODAL);
+
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
+
+    public void onAccept() throws IOException {
+        MainMenu.saveGame();
+        window.close();
+        MainMenu.window.close();
+    }
+
+    public void onDecline() {
+        window.close();
+    }
+}
